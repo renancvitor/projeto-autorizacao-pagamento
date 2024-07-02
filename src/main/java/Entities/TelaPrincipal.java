34a183e45530e98f3c6a39bb82a83ef2bb299851
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-07-02 10:09:42 -0300

    Recursos adicionais que podem ser úteis - tela principal

--- src/main/java/Entities/TelaPrincipal.java
+++ src/main/java/Entities/TelaPrincipal.java
@@ -2,10 +2,12 @@ package Entities;
 
 import Servicoes.TelaSolicitacao;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class TelaPrincipal {
     private Usuario usuario;
@@ -21,6 +23,25 @@ public class TelaPrincipal {
         Label welcomeLabel = new Label("Bem-vindo, " + usuario.getLogin() + "!");
         layout.getChildren().add(welcomeLabel);
 
+        // Resumo Rápido
+        Label resumoLabel = new Label("Resumo Rápido:");
+        Label totalPendentesLabel = new Label("Pendentes: 10");
+        Label totalAprovadasLabel = new Label("Aprovadas: 5");
+        Label totalRejeitadasLabel = new Label("Rejeitadas: 2");
+        layout.getChildren().addAll(resumoLabel, totalPendentesLabel, totalAprovadasLabel, totalRejeitadasLabel);
+
+        // Filtros de Solicitações
+        ComboBox<String> filterBox = new ComboBox<>();
+        filterBox.getItems().addAll("Todas", "Pendentes", "Aprovadas", "Rejeitadas");
+        filterBox.setValue("Todas");
+        layout.getChildren().add(filterBox);
+
+        // Barra de Pesquisa
+        TextField searchField = new TextField();
+        searchField.setPromptText("Buscar solicitações...");
+        layout.getChildren().add(searchField);
+
+        // Botão para novas solicitações
         Button novaSolicitacaoButton = new Button("Nova Solicitação");
         novaSolicitacaoButton.setOnAction(e -> {
             TelaSolicitacao telaSolicitacao = new TelaSolicitacao(usuario);
